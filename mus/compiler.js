a29d55b625d3a6860e54c421e1fd3be75b9052af
Manuel Alvarez Alvarez <chuper.man5@gmail.com>
2012-04-16 14:50:41 +0200

    Simplified compiler

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,131 +1,66 @@
 /* Compiler functions */
 var pitchMap = {
-	A: 9,
-	B: 11,
-	C: 0,
-	D: 2,
-	E: 4,
-	F: 5,
-	G: 7
+	A: 9, B: 11, C: 0, D: 2, E: 4, F: 5, G: 7
 };
 
 var convertPitch = function(pitch) {
 	return 12 + 12 * parseInt(pitch[1]) + pitchMap[pitch[0].toUpperCase()];
 };
 
-var endTime = function (expr, time) {
-	return getHandler(expr).endTime(expr, time);
-};
-
-var compileT = function(expr, time) {
-	return getHandler(expr).compileT(expr, time);
+var compileT = function(expr, time, result) {
+	var handler = handlerMap[expr.tag.toLowerCase()];
+	if(!handler) {
+	    console.log('Handler for tag \'' + expr.tag + '\' not found');
+		return time;
+	} else {
+		return handler.compileT(expr, time, result);
+	}
 };
 
 var compile = function(musexpr) {
-	return compileT(musexpr, 0);
+	var result = [];
+	compileT(musexpr, 0, result);
+	return result;
 };
 
 /* OOP Classes */
-function TagHandler() {
-};
+function NoteHandler() {};
 
-function NoteHandler() {
-};
-
-NoteHandler.prototype = new TagHandler();
-
-NoteHandler.prototype.compileT = function(expr, time) {
-	var result = [];
-	result.push(
-		{
-			tag : 'note', 
-			pitch : convertPitch(expr.pitch),
-			start : time,
-			dur: expr.dur
-		}
-	);
-	return result;
-};
-
-NoteHandler.prototype.endTime = function (expr, time) {
+NoteHandler.prototype.compileT = function(expr, time, result) {
+	result.push({tag: 'note', pitch: convertPitch(expr.pitch), start: time, dur: expr.dur});
 	return time + expr.dur;
 };
 
-function RestHandler() {
-};
+function RestHandler() {};
 
-RestHandler.prototype = new TagHandler();
-
-RestHandler.prototype.compileT = function(expr, time) {
-	var result = [];
-	result.push(
-		{
-			tag : 'rest', 
-			start : time,
-			dur: expr.dur
-		}
-	);	
-	return result;
-};
-
-RestHandler.prototype.endTime = function (expr, time) {
+RestHandler.prototype.compileT = function(expr, time, result) {
+	result.push({tag: 'rest', start: time, dur: expr.dur});	
 	return time + expr.dur;
 };
 
-function SeqHandler() {
-};
+function SeqHandler() {};
 
-SeqHandler.prototype = new TagHandler();
-
-SeqHandler.prototype.compileT = function(expr, time) {
-	var result = [];
-	result = result.concat(compileT(expr.left, time));
-	time = endTime(expr.left, time);
-	result = result.concat(compileT(expr.right, time));
-	return result;
-};
-
-SeqHandler.prototype.endTime = function (expr, time) {
-	time = endTime(expr.left, time);
-	time = endTime(expr.right, time);
+SeqHandler.prototype.compileT = function(expr, time, result) {
+	time = compileT(expr.left, time, result);
+	time = compileT(expr.right, time, result);
 	return time;
 };
 
-function ParHandler() {
+function ParHandler() {};
+
+ParHandler.prototype.compileT = function(expr, time, result) {
+	var leftTime = compileT(expr.left, time, result);
+	var rightTime = compileT(expr.right, time, result);
+	return Math.max(leftTime, rightTime);
 };
 
-ParHandler.prototype = new TagHandler();
+function RepeatHandler() {};
 
-ParHandler.prototype.compileT = function(expr, time) {
-	var result = [];
-	result = result.concat(compileT(expr.left, time));
-	result = result.concat(compileT(expr.right, time));
-	return result;
-};
-
-ParHandler.prototype.endTime = function (expr, time) {
-	var longExpr = expr.left.dur > expr.right.dur ? expr.left : expr.right;
-	time = endTime(longExpr, time);
-	return time;
-};
-
-function RepeatHandler() {
-};
-
-RepeatHandler.prototype = new TagHandler();
-
-RepeatHandler.prototype.compileT = function(expr, time) {
-	var result = [];
+RepeatHandler.prototype.compileT = function(expr, time, result) {
 	for(var i = 0; i < expr.count; i++) {
-		result = result.concat(compileT(expr.section, time));
-		time = endTime(expr.section, time);
+		time = compileT(expr.section, time, result);
 	}
-	return result;
-};
-
-RepeatHandler.prototype.endTime = function (expr, time) {
-	var newTime = endTime(expr.section, time);
-	return time + (newTime - time) * expr.count;
+	return time;
 };
 
 var handlerMap = {
@@ -135,18 +70,6 @@ var handlerMap = {
 	par: new ParHandler(),
 	repeat: new RepeatHandler()
 };
-
-var UnknownTagException = function(tag) {
-	this.tag = tag;
-}
-
-var getHandler = function(expr) {
-	var handler = handlerMap[expr.tag.toLowerCase()];
-	if(!handler) {
-		throw new UnknownTagException(expr.tag);
-	}
-	return handler;
-}
 
 /* Test methods */
 var melody_mus = 
