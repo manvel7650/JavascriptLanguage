35e1d084e065fab1074a1767f142be901b966402
unknown <malvarez@Fi2Des-Manu.(none)>
2012-04-24 18:58:02 +0200

    Added testing

--- mus/test.js
+++ mus/test.js
@@ -1,4 +1,3 @@
-var PEG = require('pegjs');
 var assert = require('assert');
 var fs = require('fs');
 var mus = require('./mus.js');
@@ -10,63 +9,52 @@ function MusTester(testSuite) {
 MusTester.prototype.execute = function() {
 	for(var i = 0; i<this.suite.length; i++) {
 		var test = this.suite[i];
+		var musexpr = test.musfile === undefined ? test.musexpr : fs.readFileSync(test.musfile, 'ascii');
 		try {
-			var musparsed = mus.parse(test.musexpr);
+			var musparsed = mus.parse(musexpr);
 			assert.deepEqual(musparsed, test.musparsed);
+		}
+		catch(err) {
+			console.log("Failed parse : " + test.message + ", cause : " + err);
+			continue;
+		}
+		try {
 			var muscompiled = mus.compile(musparsed);
 			assert.deepEqual(muscompiled, test.muscompiled);
 		}
 		catch(err) {
-			console.log("Failed : " + test.message + ", cause : " + err);
+			console.log("Failed compile : " + test.message + ", cause : " + err);
+			continue;
 		}
 	}
 };
 
 var testSuite = [
-	{musexpr : "a4:500", musparsed : {tag:'note',pitch:'a4',dur: 500}, muscompiled : [{tag:'note',pitch:69,start: 0,dur:500}], message : "Single note test"}
+	{
+		musexpr: "a4:500", 
+		musparsed: {tag:'note',pitch:'a4',dur: 500}, 
+		muscompiled: [{tag:'note',pitch:69,start: 0,dur:500}], 
+		message : "a4:500"
+	},
+	{
+		musexpr: "_:500", 
+		musparsed: {tag:'rest',dur: 500}, 
+		muscompiled: [{tag:'rest',start: 0,dur:500}], 
+		message : "_:500"
+	},
+	{
+		musexpr: "[3]a4:500", 
+		musparsed: {tag:'repeat',section:{tag:'note',pitch:'a4',dur: 500},count:3}, 
+		muscompiled: [{tag:'note',pitch:69,start: 0,dur:500},{tag:'note',pitch:69,start: 500,dur:500},{tag:'note',pitch:69,start: 1000,dur:500}], 
+		message : "[3]a4:500"
+	},
+	{
+		musfile: "test1.mus", 
+		musparsed: {tag:'seq',left:{tag:'seq',left:{tag:'note',pitch:'c3',dur: 250},right:{tag:'note',pitch:'g4',dur: 500}},right:{tag:'repeat',section:{tag:'par',left:{tag:'note',pitch:'c4',dur:250},right:{tag:'rest',dur:250}},count:3}},
+		muscompiled: [{tag:'note',pitch:48,start:0,dur:250},{tag:'note',pitch:67,start:250,dur:500},{tag:'note',pitch:60,start:750,dur:250},{tag:'rest',start:750,dur:250},{tag:'note',pitch:60,start:1000,dur:250},{tag:'rest',start:1000,dur:250},{tag:'note',pitch:60,start:1250,dur:250},{tag:'rest',start:1250,dur:250}],
+		message : "test1.mus"
+	}
 ];
 
 var musTester = new MusTester(testSuite);
 musTester.execute();
-
-
-/*
-deepEqual(mus.parse("A4[500]"), {tag:'note',pitch:'A4',dur: 500}, "A4[500]");
-deepEqual(mus.compile(mus.parse("A4[500]")), [{tag:'note',pitch:69,start: 0,dur:500}], "A4[500]");
-deepEqual(mus.parse("REST[500]"), {tag:'rest',dur: 500}, "REST[500]");
-deepEqual(mus.compile(mus.parse("REST[500]")), [{tag:'rest',start: 0,dur:500}], "REST[500]");
-deepEqual(mus.parse("SEQ{A4[500]REST[500]}"), {tag:'seq',left:{tag:'note',pitch:'A4',dur: 500},right:{tag:'rest',dur:500}}, "A4[500]\nREST[500]");
-deepEqual(mus.parse("PAR{A4[500]REST[500]}"), {tag:'par',left:{tag:'note',pitch:'A4',dur: 500},right:{tag:'rest',dur:500}}, "A4[500],REST[500]");
-deepEqual(mus.parse("3{A4[500]}"), {tag:'repeat',section:{tag:'note',pitch:'A4',dur: 500},count:3}, "3{A4[500]}");
-deepEqual(mus.parse("3  \n{\n  A4[500]\n\t}"), {tag:'repeat',section:{tag:'note',pitch:'A4',dur: 500},count:3}, "3  \n{\n  A4[500]\n\t}");
-
-var melody_mus = "C3[250],G4[500]\n3{C4[250]}\nREST[250]";
-var melody_mus_parsed = { 
-	tag: 'seq',
-	left: { 
-		tag: 'par',
-		left: { tag: 'note', pitch: 'c3', dur: 250 },
-		right:  { tag: 'note', pitch: 'g4', dur: 500 } 
-	},
-	right: { 
-		tag: 'seq',
-		left: { 
-			tag: 'repeat',
-			section: { tag: 'note', pitch: 'c4', dur: 250 },
-			count: 3
-		},
-		right: { tag: 'rest', dur: 250 }
-	}
-};
-var melody_mus_compiled = [
-	{ tag: 'note', pitch: 48, start: 0, dur: 250 },
-	{ tag: 'note', pitch: 67, start: 0, dur: 500 },
-	{ tag: 'note', pitch: 60, start: 500, dur: 250 },
-	{ tag: 'note', pitch: 60, start: 750, dur: 250 },
-	{ tag: 'note', pitch: 60, start: 1000, dur: 250 },
-	{ tag: 'rest', start: 1250, dur: 250 } 
-];
-
-deepEqual(mus.parse(melody_mus), melody_mus_parsed, "PARSE -> " + melody_mus);
-deepEqual(mus.compile(melody_mus_parsed), melody_mus_compiled, "COMPILE -> " + JSON.stringify(melody_mus, null, '\t'));
-*/
